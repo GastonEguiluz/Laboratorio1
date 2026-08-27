b14454553e6173e707eb01a024d80b56d70bc071
Luis Henderson <luis.henderson@estudiantes.utec.edu.uy>
2026-08-27 19:54:17 -0300

    Agregar avance de secuencia y antirrebote

--- parte 3/AssemblerApplication3/AssemblerApplication3/main.asm
+++ parte 3/AssemblerApplication3/AssemblerApplication3/main.asm
@@ -31,4 +31,52 @@ RESET:
     clr patron
 
 PRINCIPAL:
+    rcall LEER_BOTONES
+    rcall EJECUTAR_SECUENCIA
     rjmp PRINCIPAL
+
+EJECUTAR_SECUENCIA:
+    ret
+
+LEER_BOTONES:
+    sbis PINB, PB0
+    rjmp BOTON_SIGUIENTE
+    ret
+
+BOTON_SIGUIENTE:
+    rcall RETARDO_ANTIRREBOTE
+    sbic PINB, PB0
+    rjmp FIN_LEER_BOTONES
+    inc secuencia
+    cpi secuencia, 8
+    brlo SIGUIENTE_LISTA
+    clr secuencia
+
+SIGUIENTE_LISTA:
+    clr patron
+
+ESPERAR_SIGUIENTE:
+    sbis PINB, PB0
+    rjmp ESPERAR_SIGUIENTE
+    rcall RETARDO_ANTIRREBOTE
+
+FIN_LEER_BOTONES:
+    ret
+
+RETARDO_ANTIRREBOTE:
+    ldi retardo1, 2
+
+ANTIRREBOTE_1:
+    ldi retardo2, 255
+
+ANTIRREBOTE_2:
+    ldi retardo3, 255
+
+ANTIRREBOTE_3:
+    dec retardo3
+    brne ANTIRREBOTE_3
+    dec retardo2
+    brne ANTIRREBOTE_2
+    dec retardo1
+    brne ANTIRREBOTE_1
+    ret
